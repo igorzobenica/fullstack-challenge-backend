907c0d9db818150c9b37fd0fb3b316345fb3ece1
Igor Zobenica <igor.zobenica@gmail.com>
2024-09-05 14:04:07 +0200

    Enable cors for deployed frontend app

--- functions/src/middlewares/corsMiddleware.ts
+++ functions/src/middlewares/corsMiddleware.ts
@@ -1,17 +1,20 @@
 import cors from 'cors'
+import * as functions from 'firebase-functions';
+
+const frontendUrl = functions.config().app?.frontend_url || 'http://localhost:5173';
 
 const corsHandler = cors({
   origin: (origin, callback) => {
     const allowedOrigins = [
-      'http://localhost:5173', // TODO: Add production URL once FE gets deployed,
-    ]
+      frontendUrl,
+    ];
 
     if (allowedOrigins.includes(origin ?? '') || !origin) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
   },
-})
+});
 
 export default corsHandler
